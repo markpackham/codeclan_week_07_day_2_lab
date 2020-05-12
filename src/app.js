6bddb181c61aac2014c1936e434a4c4e193afe4c
mark <markpackham1@gmail.com>
2020-05-12 15:45:19 +0100

    Add flag

--- src/app.js
+++ src/app.js
@@ -7,7 +7,9 @@ document.addEventListener('DOMContentLoaded', () => {
     el: '#app',
     data: {
             countries: [],
-            filterCountry: ""
+            filterCountry: "",
+            favCountry: [],
+            flagImgURL: null
     },
     mounted(){
         this.fetchCountries();
@@ -20,7 +22,14 @@ document.addEventListener('DOMContentLoaded', () => {
               .then(countries => this.countries = countries)
           },
         filteredCountries: function(){
-            return this.countries.filter(country => country.name === this.filterCountry)
+            return this.countries.filter((country) => {
+                //this.flagImgURL = country.flag;
+                return country.name === this.filterCountry;
+                
+            })   
+          },
+          addFavCountry: function(favCountry){
+              this.favCountry.push(favCountry);
           }
     }
   });
@@ -37,3 +46,31 @@ document.addEventListener('DOMContentLoaded', () => {
 // filteredAccounts: function(){
 //     return this.accounts.filter(account => account.balance >= this.filterAmount);
 //   }
+
+// const hardCodedFlag = "https://upload.wikimedia.org/wikipedia/en/thumb/c/c3/Flag_of_France.svg/250px-Flag_of_France.svg.png";
+
+// flagMaker: function(){
+//     return `<img src ="${hardCodedFlag}" />`
+// }
+
+// const createListItem = function(animal) {
+//     const li = document.createElement('li');
+//     li.textContent = animal.name;
+  
+//     const speciesSpan = document.createElement('span');
+//     speciesSpan.classList.add('species');
+//     speciesSpan.textContent = animal.species;
+//     li.appendChild(speciesSpan);
+  
+//     const continentSpan = document.createElement('span');
+//     continentSpan.textContent = animal.continent;
+//     continentSpan.classList.add('continent');
+//     li.appendChild(continentSpan);
+  
+//     return li;
+//   };
+  
+//   const addListItem = function(listItem) {
+//     const listContainer = document.querySelector('#list');
+//     listContainer.appendChild(listItem);
+//   };
